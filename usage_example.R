a936e73ff5d2f33aa179fb950215d7bb22ca6933
mthane <44970717+mthane@users.noreply.github.com>
2022-02-25 12:22:10 +0100

    Add files via upload

--- usage_example.R
+++ usage_example.R
@@ -1,6 +1,5 @@
-library(devtools)
-load_all()
-
+library(ggradial)
+library(tidyverse)
 mock = create_mockdata(Nf=25,n_treatments = 2,ngroups = 5)
 dfs = mock$dfs
 grs = mock$grs
@@ -15,11 +14,11 @@ data_cluster <- df %>% mutate(.cluster = fct_drop(as.factor(.cluster))) %>% sele
 
 group_names = grs$.gr_name[grs$.gr_id != 1]
 #
-# radial_barchart_post_treatment(data_cluster,group_names)
-#
+
 data_static = data_cluster %>%select(!c(".id",".phase"))
 radial_barchart_static(data_static, group_names,interactive = F)
-#ggsave("test_plot.png")
-#
-# data_linechart = df%>%select(!c(".phase"))
-# radial_line_chart(data_linechart,group_names)
+
+data_linechart = df%>%select(!c(".phase"))
+radial_line_chart(data_linechart,group_names)
+
+radial_barchart_post_treatment(data_cluster,group_names)
